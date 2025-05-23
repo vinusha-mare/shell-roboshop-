4ed21c7874f6438a54d26b2f2333bddf8eb218f6
vinusha <vinushasaimare@gmail.com>
2025-05-23 13:23:49 +0530

    hello world started

--- roboshop.sh
+++ roboshop.sh
@@ -26,7 +26,7 @@ do
         ,"Changes": [{
         "Action"              : "UPSERT"
         ,"ResourceRecordSet"  : {
-            "Name"              : "'$instance'",'$DOMAIN_NAME'"
+            "Name"              : "'$instance'".'$DOMAIN_NAME'"
             ,"Type"             : "A"
             ,"TTL"              : 1
             ,"ResourceRecords"  : [{
