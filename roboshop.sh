8c11c980c4b9c86a6a52db6deac1c6a2ef53da13
vinusha <vinushasaimare@gmail.com>
2025-05-23 13:29:37 +0530

    hello world started

--- roboshop.sh
+++ roboshop.sh
@@ -22,15 +22,15 @@ do
    --hosted-zone-id $ZONE_ID \
    --change-batch '
    {
-        "Comment": "Creating or Updating a record set for cognito endpoint"
+        "Comment": "Creating or Updating a record set for $instance"
         ,"Changes": [{
         "Action"              : "UPSERT"
         ,"ResourceRecordSet"  : {
-            "Name"              : "'$instance'".'$DOMAIN_NAME'"
+            "Name"              : "$instance.$DOMAIN_NAME"
             ,"Type"             : "A"
             ,"TTL"              : 1
             ,"ResourceRecords"  : [{
-                "Value"         : "'$IP'"
+                "Value"         : "$IP"
             }]
  
         }
