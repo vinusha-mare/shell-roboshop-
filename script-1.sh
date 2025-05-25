98210921671516196d1e0484f21ca14017371434
vinusha <vinushasaimare@gmail.com>
2025-05-25 17:29:45 +0530

    roboshop

--- script-1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash 
-
-GREETING="Hello, Good Morning"
-echo "$GREETING"
-echo "PID of SCRIPT-1: $$"
-
-./script-2.sh
--- script-2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "PID of SCRIPT-2: $$"
-
-echo "$GREETING"
